b73e73457e23ecf50c9a1cd47eea2763bd16b735
karenjli <karenjy.li@gmail.com>
2019-09-06 10:51:23 -0500

    data fetched and displayed

--- client/src/components/player.js
+++ client/src/components/player.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-exoirt default function Player (props) {
-    return (
-        <div className="player">
-            <h2>Name: {props.name}</h2>
-            <h4>Country: {props.country}</h4>
-            <h4>Searches: {props.searches} </h4>
-        </div>
-    )
+export default function Player(props) {
+  return (
+    <div className="player">
+      <h2>Name: {props.name}</h2>
+      <h4>Country: {props.country}</h4>
+      <h4>Searches: {props.searches} </h4>
+    </div>
+  );
 }
